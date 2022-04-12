9e8639ad79c80019f3c2745596baaf8b838f46c5
jamessteven74 <jamesstevenvictorien@gmail.com>
2022-04-12 13:54:39 +0200

    initial_commit

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+//Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+
+
+// id del post, numero progressivo da 1 a n
+// nome autore,
+// foto autore,
+// data in formato americano (mm-gg-yyyy),
+// testo del post,
+// immagine (non tutti i post devono avere una immagine),
+// numero di likes.
+
+
+
+
+
+let posts = [{
+
+    "id": 1,
+    "autore": "mario",
+    "foto" : "mario_jpg",
+    "data" : "12-13-2016",
+    "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, voluptatum!",
+    "img": "sunset.jpg",
+    "likes": 100,
+
+
+
+
+
+
+
+
+},
+
+{
+
+    "id": 1,
+    "autore": "giovanni",
+    "foto" : "mario_jpg",
+    "data" : "12-13-2016",
+    "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, voluptatum!",
+    "img": "sunset.jpg",
+    "likes": 80,
+
+
+
+
+
+
+
+
+}
+
+]
